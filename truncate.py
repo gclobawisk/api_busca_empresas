1e8bc9fae5f86fca181d58522239f47da9605427
Gabriel Rodrigues <gabriel.clobawisk@hotmail.com>
2023-12-24 16:04:46 -0300

    att lista2

--- truncate.py
+++ truncate.py
@@ -11,38 +11,38 @@ def truncate_reset_auto_increment(tabela):
 db = Database()
 
 # APAGAR TODOS OS REGISTROS DA TABELA E RESETAR O ÍNDICE DO ID AUTO INCREMENT
-retorno_zerar_tabela = truncate_reset_auto_increment("atividades_secundarias")
+#retorno_zerar_tabela = truncate_reset_auto_increment("atividades_secundarias")
 print('retorno_zerar_tabela atividades_secundarias: {}'.format(retorno_zerar_tabela))
 
-retorno_zerar_tabela = truncate_reset_auto_increment("atividade_principal")
+#retorno_zerar_tabela = truncate_reset_auto_increment("atividade_principal")
 print('retorno_zerar_tabela atividade_principal: {}'.format(retorno_zerar_tabela))
 
-retorno_zerar_tabela = truncate_reset_auto_increment("empresas")
+#retorno_zerar_tabela = truncate_reset_auto_increment("empresas")
 print('retorno_zerar_tabela empresa: {}'.format(retorno_zerar_tabela))
 
-retorno_zerar_tabela = truncate_reset_auto_increment("socios")
+#retorno_zerar_tabela = truncate_reset_auto_increment("socios")
 print('retorno_zerar_tabela socios: {}'.format(retorno_zerar_tabela))
 
-retorno_zerar_tabela = truncate_reset_auto_increment("contatos")
+#retorno_zerar_tabela = truncate_reset_auto_increment("contatos")
 print('retorno_zerar_tabela contatos: {}'.format(retorno_zerar_tabela))
 
-retorno_zerar_tabela = truncate_reset_auto_increment("enderecos")
+#retorno_zerar_tabela = truncate_reset_auto_increment("enderecos")
 print('retorno_zerar_tabela enderecos: {}'.format(retorno_zerar_tabela))
 
-retorno_zerar_tabela = truncate_reset_auto_increment("anexos_solicitacoes_gerenciar_empresa")
+#retorno_zerar_tabela = truncate_reset_auto_increment("anexos_solicitacoes_gerenciar_empresa")
 print('retorno_zerar_tabela anexos_solicitacoes_gerenciar_empresa: {}'.format(retorno_zerar_tabela))
 
-retorno_zerar_tabela = truncate_reset_auto_increment("mensagens")
+#retorno_zerar_tabela = truncate_reset_auto_increment("mensagens")
 print('retorno_zerar_tabela mensagens: {}'.format(retorno_zerar_tabela))
 
-retorno_zerar_tabela = truncate_reset_auto_increment("respostas_mensagens")
+#retorno_zerar_tabela = truncate_reset_auto_increment("respostas_mensagens")
 print('retorno_zerar_tabela respostas_mensagens: {}'.format(retorno_zerar_tabela))
 
-retorno_zerar_tabela = truncate_reset_auto_increment("solicitacoes_gerenciar_empresa")
+#retorno_zerar_tabela = truncate_reset_auto_increment("solicitacoes_gerenciar_empresa")
 print('retorno_zerar_tabela solicitacoes_gerenciar_empresa: {}'.format(retorno_zerar_tabela))
 
-retorno_zerar_tabela = truncate_reset_auto_increment("usuarios_empresas")
-print('retorno_zerar_tabela usuarios_empresas: {}'.format(retorno_zerar_tabela))
+#retorno_zerar_tabela = truncate_reset_auto_increment("usuarios_empresas")
+#print('retorno_zerar_tabela usuarios_empresas: {}'.format(retorno_zerar_tabela))
 
 
 
